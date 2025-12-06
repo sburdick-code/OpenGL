c172b66888ea2cd0855c724c50ff60c390042c3b
Simon <burdicksimon@gmail.com>
2025-12-06 15:35:11 -0800

    2) Shader Creation
    https://www.youtube.com/watch?v=HdNaUwYc8_A&list=PLn3eTxaOtL2PHxN8EHf-ktAcN-sGETKfw&index=3

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,16 @@
 
 int main() 
 {
+
+    // Read in text file
+    std::ifstream file;
+    std::string line;
+
+    file.open(SOURCE_DIR + "shaders/vertex.txt");
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+
     GLFWwindow* window;
 
     // Ensure GLFW initializes successfully
@@ -34,4 +44,35 @@ int main()
     }
 
     return 0;
+}
+
+// Generate a fragment shader, vertex shader, etc.
+unsigned int make_module(const std::string& filepath, unsigned int module_type) {
+    std::ifstream file;
+    std::stringstream bufferedLines;
+    std::string line;
+
+    file.open(filepath);
+    while (std::getline(file, line)) {
+        bufferedLines << line << "\n";
+    }
+
+    std::string shaderSource = bufferedLines.str();
+    const char* shaderSrc = shaderSource.c_str();
+    bufferedLines.str("");
+    file.close();
+
+    unsigned int shaderModule = glCreateShader(module_type);
+    glShaderSource(shaderModule, 1, &shaderSrc, NULL);
+    glCompileShader(shaderModule);
+
+    int success;
+    glGetShaderiv(shaderModule, GL_COMPILE_STATUS, &status);
+    if (!success) {
+        char errorLog[1024];
+        glGetShaderInfoLog(shaderModule, 1024, errorLog);
+        std::cout << "Shader Module compilation error:\n" << errorLog << std::endl;
+    }
+
+    return shaderModule;
 }
